2a55120138bcfe10b655e210e721792c2bfc957c
hackers365 <hackers365@gmail.com>
2022-09-14 11:19:18 +0800

    Update redis_metrics_v8.go

--- redis_metrics/redis_metrics_v8.go
+++ redis_metrics/redis_metrics_v8.go
@@ -12,13 +12,11 @@ type RedisV8Hook struct {
 }
 
 func (r *RedisV8Hook) BeforeProcess(ctx context.Context, cmd redis.Cmder) (context.Context, error) {
-	fmt.Println("BeforeProcess")
 	nCtx := context.WithValue(ctx, "ts", time.Now())
 	return nCtx, nil
 }
 
 func (r *RedisV8Hook) AfterProcess(ctx context.Context, cmd redis.Cmder) error {
-	fmt.Println("AfterProcess")
 	if v := ctx.Value("ts"); v != nil {
 		start := v.(time.Time)
 		addRedisPrometheusLabelValue(cmd.Name(), float64(time.Since(start).Nanoseconds())/1e6)
